a814774e965f005dfed14cde9a7dfcbe348dc5a6
Justin Abellera <justin.c.abellera@gmail.com>
2022-07-26 22:51:47 -0700

    styling done

--- src/Form.js
+++ src/Form.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './Form.css'
 
 export default function Form(props) {
 const {name, email, role} = props;
@@ -8,35 +9,41 @@ const onChange = e => {
 }
 
 return (
-<form>
-    <label>Name
-        <input
-            value={name} 
-            name='name' 
-            type='text' 
-            placeholder='enter name'
-            onChange={onChange}
-        />
-    </label>
+<div className='Form'>
+    <form>
+        <label>Name
+            <input
+                value={name} 
+                name='name' 
+                type='text' 
+                placeholder='enter name'
+                onChange={onChange}
+            />
+        </label>
 
-    <label>Email
-        <input 
-            value={email}
-            name='email' 
-            type='email' 
-            placeholder='enter email'
-            onChange={onChange}
-        />
-    </label>
+        <label>Email
+            <input 
+                value={email}
+                name='email' 
+                type='email' 
+                placeholder='enter email'
+                onChange={onChange}
+            />
+        </label>
 
-    <label>Role
-        <select value={role} name='role' onChange={onChange}>
-            <option value=''>-- What do you do? --</option>
-            <option value='wizard'>I'm a wizard, Harry</option>
-            <option value='jedi'>I cut paper with a lightsaber</option>
-            <option value='flash'>I run fast, don't get in my way</option>
-        </select>
-    </label>
-</form>
+        <label>Role
+            <select value={role} name='role' onChange={onChange}>
+                <option value=''>-- What do you do? --</option>
+                <option value='wizard'>I'm a wizard, Harry</option>
+                <option value='jedi'>I cut paper with a lightsaber</option>
+                <option value='flash'>I run fast, don't get in my way</option>
+            </select>
+        </label>
+
+        <div className='submit'>
+            <button>submit</button>
+        </div>
+    </form>
+</div>
     )
 }
